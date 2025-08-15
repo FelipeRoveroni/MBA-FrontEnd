7fdef5d3151a172d4c54271319ad3ab5a21a3172
FelipeRoveroni <feliperoveroni541@gmail.com>
2025-08-15 08:15:06 -0300

    Bloco01 finalizado. Iniciando Bloco02

--- Aula02/src/projeto-1/app.js
+++ Aula02/src/projeto-1/app.js
@@ -3,3 +3,5 @@ document.querySelector('h1').textContent = 'ESALQ'
 document.title = 'MBA ESALQ'
 
 let tagH3 = document.createElement('h3')
+tagH3.textContent = 'Engenharia de Software'
+document.body.appendChild(tagH3)
